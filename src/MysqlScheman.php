98114ebdb8019b1dc0a31977b1033274ce42ff9d
Shabeer <shabeer.ali@dynacommerce.com>
2020-10-18 01:22:42 +0530

    Base

---
+++ src/MysqlScheman.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * MysqlScheman.php
+ *
+ * @copyright      MIT
+ * @author         
+ * @since          0.0.1
+ *
+ */
+
+namespace MysqlScheman;
+
+/**
+ * MysqlScheman Class
+ */
+class MysqlScheman
+{
+    public static function test()
+    {
+        echo 'test';
+    }
+}
